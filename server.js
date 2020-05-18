be1bf00c45f3b21db55b69f5e3f3e13d50e820a0
unknown <davidmshayne.jftd@gmail.com>
2020-05-18 11:32:36 +0200

    added other client files

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/users', userAPI);
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
