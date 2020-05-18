75d7eb45f6db4f46a87334e3ec4b791f68efb6ce
unknown <davidmshayne.jftd@gmail.com>
2020-05-18 13:41:49 +0200

    Build in client

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const app = express();
-// const keys = require('./config/keys');
-// const userAPI = require('./routes/userAPI');
+const keys = require('./config/keys');
+const userAPI = require('./routes/userAPI');
 const path = require('path');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// app.use(express.json());
+app.use(express.json());
 
-// mongoose
-//     .connect(keys.MONGO_URI, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })
-//     .then(console.log('MongoDB connected'))
-//     .catch(err => {
-//         throw err;
-//     });
+mongoose
+    .connect(keys.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log('MongoDB connected'))
+    .catch(err => {
+        throw err;
+    });
 
-// app.use('/api/users', userAPI);
+app.use('/api/users', userAPI);
 
-app.use(express.static('build'));
+app.use(express.static('client/build'));
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
